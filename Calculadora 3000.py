bf7135d377e8ce5841cec7e69b90ab93ee17ce45
Mr_Code <118413944+MrCodeJJ@users.noreply.github.com>
2023-04-20 21:01:23 -0500

    Create Calculadora 3000.py
    Unico Problema El programa se cierra al instante despues de Multiplicar los numeros

---
+++ Calculadora 3000.py
@@ -0,0 +1,9 @@
+#Calculadora Basica 20/04/23
+
+print("    Bienvenido a la calculadora 3000     ")
+
+Num1=input(" Favor de introducir el primer digito: ")
+Num2=input(" Favor de introducir el segundo digito: ")
+Total=Num1*Num2
+
+print(" Tu Resultado es: "+ Total) # Unico Problema El programa se cierra al instante despues de Multiplicar los numeros
